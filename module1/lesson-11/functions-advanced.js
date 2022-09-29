350c752ffef1f3b53e87ef1ec0bc446378a1887d
LloydC <lloydchambrier@gmail.com>
2022-09-29 20:39:44 +0200

    chore: lesson-11 code add

--- module1/lesson-11/functions-advanced.js
+++ module1/lesson-11/functions-advanced.js
@@ -1,34 +1,58 @@
+
+console.log(calcSum(12, 23));
 // Function declaration
 function calcSum(x, y) {
     return x + y;
-  }
+}
    
-  calcSum(12, 23);
 
 // Function expression
-
-const calcSum = function (x, y){
+const calcSumTwo = function (x, y){
     return x + y;
 }
 
+console.log(calcSumTwo(12,24))
+
 // Callback 
 
-function eatDinner(callback) {
+function eatDinner(callback, dessertName) {
     console.log('Eating the dinner!');
-    callback();
+    callback(dessertName);
 }
    
-function eatDessert() {
-    console.log('Eating the dessert!');
+function eatDessert(dessertName) {
+    console.log('Eating the dessert!' + dessertName);
 }
 
-eatDinner(eatDessert);
+eatDinner(eatDessert, 'rice pudding');
 
 // Anonymous functions
 function printName(name, anonFunc) {
     anonFunc(name);
-  }
+}
    
 printName('sandra', function (name) {
     console.log(name[0].toUpperCase() + name.slice(1));
 });
+
+// Arrow functions
+// function calculateSum(x, y) {
+//     return x + y;
+// }
+
+const calculateSum = (x, y) => { return x + y;}
+
+// const calculateSumTwo = function (x, y){
+//     return x + y;
+// }
+
+const calculateSumTwo = (x, y) => x + y;
+
+// function eatingDinner(callback, dessertName) {
+//     console.log('Eating the dinner!');
+//     callback(dessertName);
+// }
+
+const eatingDinner = (callback, dessertName) => callback(dessertName);
+
+eatingDinner(eatDessert, 'chocolate mousse')
--- module1/lesson-11/scope.js
+++ module1/lesson-11/scope.js
@@ -4,6 +4,40 @@ function sayHelloFromLocalScope() {
   const greeting = 'Hello from functional/local scope!';
   return greeting;
 }
+
+for(let i = 0; i < message.length; i++){
+
+//  console.log(testScope)
+}
+
+// console.log(testScope); 
+// console.log(greeting);
+
+// this keyword
+
+const user = {
+  name: 'ana',
+  age: 29,
+  getOlder: function () {
+    setTimeout(() => {
+      console.log(this)
+      // console.log(this);
+      // console.log(this.name);
+    }, 1000)
+  }
+};
  
-console.log(message); 
-console.log(greeting);
+// user.getOlder();
+
+let a = 1;
+let b = 2;
+ 
+function inner() {
+  a = 4; // reassigned
+  let b = 3; // declared in an inner scope
+  // console.log(b)
+}
+
+inner();
+;// => 4
+console.log(b);
