27618a7843a0c68a728ec20dcc378d7a6215fa40
LloydC <lloydchambrier@gmail.com>
2022-10-25 21:13:34 +0200

    chore: node-http-server folder add

--- module2/lesson-01/my-first-node/app.js
+++ module2/lesson-01/my-first-node/app.js
@@ -1,4 +1,13 @@
-const colors = require('colors'); // loading the 'colors' package
+const myColors = require("colors/safe");
+const dogNames = require('dog-names');
+ 
+// console.log(myColors.yellow('hello')); // outputs green text
+// console.log(myColors.red.underline('i like cake and pies')) // outputs red underlined text
+// console.log(myColors.inverse('inverse the color')); // inverses the color
+// console.log(myColors.rainbow('OMG Rainbows!')); // outputs text in colors of a rainbow
+// console.log(myColors.trap('Run the trap')); // Outputs the text using the trap font
 
-console.log('hello'.green); // outputs green text
-console.log('i like cake and pies'.underline.red);
+const randomFemaleName =  dogNames.femaleRandom();
+const randomMaleName = dogNames.maleRandom();
+
+console.log(randomMaleName, randomFemaleName)
---
+++ module2/lesson-01/node-http-server/app.js
@@ -0,0 +1,22 @@
+const http = require('http'); // loads the in-built http package
+const server = http.createServer((request, response) => {
+    if (request.url === '/') {
+        response.write('Welcome to the Homepage');
+        response.end();
+      } 
+      else if(request.url === '/about'){
+        response.write('Welcome to the About page');
+        response.end();
+      }
+      else if(request.url === '/contact'){
+        response.write('Welcome to the Contact page');
+        response.end();
+      }
+      else {
+        response.statusCode = 404;
+        response.write('404 Page');
+        response.end();
+      }
+  }); // creates my server
+
+server.listen(3000, ()=> console.log('Server is running on port 3000')); // listen to incoming requests on port 3000
